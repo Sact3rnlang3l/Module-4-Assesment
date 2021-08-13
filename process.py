d48e0d9bdcd75f2d6188656cd85f8fd67dd1b432
Aiden gillespie <aiden@cold.org>
2021-08-13 09:49:47 -0600

    Second commit, maybe I understand python more than I thought?

--- process.py
+++ process.py
@@ -1,11 +1,11 @@
 log_file = open("um-server-01.txt")
+#opens and logs the entire um-server-01 file in console
 
-
-def sales_reports(log_file):
-    for line in log_file:
-        line = line.rstrip()
-        day = line[0:3]
-        if day == "Tue":
+def sales_reports(log_file): # creates a function called sales_reports using the parameter of log_file
+    for line in log_file: #let's make the parameter into a loop
+        line = line.rstrip() #line strips anything extra in a string
+        day = line[0:3] #day is two specific parts of the list , 0 and 3 in the index
+        if day == "Mon": #if the day is Mon print the line
             print(line)
 
 
